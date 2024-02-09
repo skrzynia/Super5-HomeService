bb495546fb73a6d9d919578c47639a0c216d3ed6
Wojciech Skrzyński <skrzynia777@gmail.com>
2024-02-09 16:04:11 +0000

    Second Commit
    Source code added

---
+++ src/main/java/com/heroservice/heroservice/HeroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.heroservice.heroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heroservice/heroservice/HeroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroservice.heroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
